fac27ccbb9ffe20f411fc7eb7b6e645ed1fc508a
MyriadMinds <MindsMyriad@gmail.com>
2024-05-02 16:34:38 +0200

    moved shape hizmo plugin into png

--- BeepoAvatarPNG/BeepoAvatarPNGPlugin.cs
+++ BeepoAvatarPNG/BeepoAvatarPNGPlugin.cs
@@ -7,15 +7,19 @@ using System.Net.NetworkInformation;
 public partial class BeepoAvatarPNGPlugin : EditorPlugin
 {
   // public static readonly string SingletonName = "BeepoAvatarPNGPlugin";
+  private ShapeGizmo gizmo;
 
   public override void _EnterTree()
   {
     // AddAutoloadSingleton(SingletonName, "./BeepoCore.tscn");
+    gizmo = new(GetUndoRedo());
+    AddNode3DGizmoPlugin(gizmo);
   }
 
   public override void _ExitTree()
   {
     // RemoveAutoloadSingleton(SingletonName);
+    RemoveNode3DGizmoPlugin(gizmo);
   }
 }
 #endif
